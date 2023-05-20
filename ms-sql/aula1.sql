21b1129531821006b4356f9d44b4d426c3c66a87
ANDRÉ HENRIQUE MARFIN FERREIRA <3277.343@edu.fb.org.br>
2023-05-19 21:58:40 -0300

    texto

---
+++ ms-sql/aula1.sql
@@ -0,0 +1,8 @@
+use AdventureWorks2012
+
+select * from sys.tables
+select * from Person.Person
+select FirstName from Person.Person
+
+select FirstName as PrimeiroNome, LastName as UltimoNome from Person.Person 
+
